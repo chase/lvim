fca9cfc68cbe166c8928c57206e75077418568e6
Chase Colman <chase@colman.io>
2021-11-15 16:09:00 +0800

    Update lsp_signature

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -31,25 +31,27 @@ M.config = function ()
       config = function()
         local cfg = {
           bind = true,
-          doc_lines = 10,
-          floating_window = false, -- show hint in a floating window, set to false for virtual text only mode
+          doc_lines = 5,
+          floating_window = true, -- show hint in a floating window, set to false for virtual text only mode
+          floating_window_above_cur_line = true,
           fix_pos = false, -- set to true, the floating window will not auto-close until finish all parameters
-          hint_enable = true, -- virtual hint enable
+          hint_enable = false, -- virtual hint enable
           hint_prefix = "  ", -- Panda for parameter
           hint_scheme = "String",
           -- use_lspsaga = false, -- set to true if you want to use lspsaga popup
           hi_parameter = "Search", -- how your parameter will be highlight
-          max_height = 5, -- max height of signature floating_window, if content is more than max_height, you can scroll down
+          max_height = 8, -- max height of signature floating_window, if content is more than max_height, you can scroll down
           -- to view the hiding contents
-          max_width = 80, -- max_width of signature floating_window, line will be wrapped if exceed max_width
+          max_width = 120, -- max_width of signature floating_window, line will be wrapped if exceed max_width
           handler_opts = {
-            border = "none",   -- double, single, shadow, none
+            border = "rounded",   -- double, single, shadow, none
           },
-          -- extra_trigger_chars = {"(", ",", "{"}, -- Array of extra characters that will trigger signature completion, e.g., {"(", ","}
+          extra_trigger_chars = {"(", ",", "{"}, -- Array of extra characters that will trigger signature completion, e.g., {"(", ","}
           zindex = 200, -- by default it will be on top of all floating windows, set to 50 send it to bottom
           debug = false, -- set to true to enable debug logging
           log_path = "debug_log_file_path", -- debug log path
           padding = "", -- character to pad on left and right of signature can be ' ', or '|'  etc
+          timer_interval = 100,
         }
         require "lsp_signature".setup(cfg)
       end
